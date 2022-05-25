d73f75508bee2a91cfc995b44ff6a0fa2cb70976
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2022-05-26 03:40:02 +0800

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeroBannerComponent } from './pages/home/sections/hero-banner/hero-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroBannerComponent
   ],
   imports: [
     BrowserModule,
