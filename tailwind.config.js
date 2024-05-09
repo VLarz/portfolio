03cb0889013c320e398b6ca94fd6d3cb1976c96a
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2024-05-10 00:35:58 +0800

    inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1440px',
+      },
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
+    colors: {
+      black: `#040404`,
+      grey: `#A8A8A8`,
+    },
   },
   plugins: [],
 };
