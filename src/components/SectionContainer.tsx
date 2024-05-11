2c6734fceabe54ddcfd7bc7eb2d993dd35811af5
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2024-05-12 00:55:09 +0800

    adjust section container padding horizontal

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -6,7 +6,7 @@ export default function SectionContainer({
   children,
 }: Props) {
   return (
-    <div className={`max-w-8xl mx-auto px-32 py-16 ${className}`} style={style}>
+    <div className={`max-w-8xl mx-auto px-16 py-16 ${className}`} style={style}>
       {children}
     </div>
   );
