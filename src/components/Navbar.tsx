0ca35b23886f324f330f9d64a86e9589c8a8f9d3
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2024-05-15 00:03:36 +0800

    add temporary logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,11 +19,13 @@ const navMenu: NavMenu[] = [
 export default function Navbar({}: Props) {
   return (
     <nav className={`flex justify-center items-center py-8`}>
-      <div className={`max-w-8xl px-6 sm:px-8 lg:px-16 flex justify-between w-full`}>
-        <h1 className={`font-bold text-2xl`}>VLarz</h1>
-        <ul className={`flex gap-x-6`}>
+      <div
+        className={`max-w-8xl px-6 sm:px-8 lg:px-16 flex justify-between w-full`}
+      >
+        <img src="/logo.svg" alt="Vhen Larson Dela Cuesta" className={`w-32`} />
+        <ul className={`flex items-center gap-x-6`}>
           {navMenu.map((menu, index) => (
-            <li key={index} className={`font-semibold`}>
+            <li key={index} className={`text-sm lg:text-base font-semibold`}>
               <a href={menu.url}>{menu.label}</a>
             </li>
           ))}
