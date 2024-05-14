77649e9dc43c754186724591c4e6c95d93f36269
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2024-05-14 17:05:15 +0800

    update projects description and fix the sorting

--- src/layouts/Projects/data.ts
+++ src/layouts/Projects/data.ts
@@ -19,8 +19,17 @@ export const projectsData: Project[] = [
     },
   },
   {
+    title: `Pristine`,
+    description: `Bitcoin-backed loans platform that features Bitcoin Mortgage, Multi-sig Loans, and Trustless Lightning Loans.`,
+    image: `/images/projects/pristine.webp`,
+    links: {
+      web: ``,
+      figma: `https://www.figma.com/file/11rGtHrWZ12ZyRi6MzckQd/Pristine?type=design&node-id=0%3A1&mode=design&t=2pj9zw0YMTHeCIX5-1`,
+    },
+  },
+  {
     title: `BitLink`,
-    description: `lorem ipsum`,
+    description: `A fully functioning Bitcoin(BTC) web wallet accessed via link (URL). Think of it like a gift card, red envelope, 红包 hóngbāo or laisee.`,
     image: `/images/projects/bitlink.webp`,
     links: {
       web: ``,
@@ -28,12 +37,12 @@ export const projectsData: Project[] = [
     },
   },
   {
-    title: `coffee:life`,
-    description: `lorem ipsum`,
-    image: `/images/projects/coffee-life.webp`,
+    title: `CopperMask`,
+    description: `A sleek online hub to buy premium masks effortlessly. Browse, select, and purchase with ease. Welcome to our streamlined mask marketplace.`,
+    image: `/images/projects/coppermask.webp`,
     links: {
-      web: `https://www.coffeelife.ph/`,
-      figma: `https://www.figma.com/file/LVf08ixx7AReDOX75YESSD/Coffee-Life-Website?type=design&node-id=0-1&mode=design&t=TXNaSrMf7ad9T7ha-0`,
+      web: `http://www.coppermask.ph/`,
+      figma: `https://www.figma.com/file/GuksEJW3TDCPgpnbf4gz6N/CopperMask-Website-Revamp?type=design&node-id=0-1&mode=design`,
     },
   },
   {
@@ -46,21 +55,104 @@ export const projectsData: Project[] = [
     },
   },
   {
+    title: `Quest:Citizens`,
+    description: `A collection of 2,500 Quest: Citizens represent the adventure to frontiers equipped with new powers never before seen in Solana.`,
+    image: `/images/projects/quest-citizens.webp`,
+    links: {
+      web: `https://citizens.thequesthub.io/`,
+      figma: ``,
+    },
+  },
+
+  {
+    title: `toktok`,
+    description: `An app designed to connect people through door-to-door deliveries and promote easier online shopping.`,
+    image: `/images/projects/toktok.webp`,
+    links: {
+      web: `https://toktok.ph/`,
+      figma: `https://www.figma.com/design/UsmhBDIK2sFzzJpRzByjRL/toktoksuper-Website?node-id=0%3A1&t=vsK2QxOdJaFTicPM-1`,
+    },
+  },
+  {
+    title: `toktokmall`,
+    description: `Shopping is something that most Filipinos love. From healthcare to essentials and everything in between, toktokmall is now here! Experience hassle-free shopping with toktokmall.`,
+    image: `/images/projects/toktokmall.webp`,
+    links: {
+      web: `https://toktokmall.ph/`,
+      figma: `https://www.figma.com/design/yU3uAQJrORAb0fdjisrHHp/toktokmall-web?node-id=1%3A148&t=lBwz46xx5dl4gmfe-1`,
+    },
+  },
+  {
+    title: `toktokfood`,
+    description: `Toktokfood, developed by toktok, caters to Filipinos' love for food. Similar to other food delivery services, toktokfood satisfies your cravings with just a few clicks, delivering food straight to your doorstep.`,
+    image: `/images/projects/toktokfood.webp`,
+    links: {
+      web: `https://toktokfood.ph/`,
+      figma: ``,
+    },
+  },
+  {
+    title: `toktokwallet`,
+    description: `Toktokwallet offers a safe, reliable, and fast way to handle transactions. It allows easy fund transfers between your bank accounts and toktokwallet, and can be used for toktokfood, toktokmall, and toktok delivery transactions.`,
+    image: `/images/projects/toktokwallet.webp`,
+    links: {
+      web: `https://wallet.toktok.ph/`,
+      figma: `https://www.figma.com/design/F8pniqNOskEKk7FRCOAcZz/toktokwallet-Website?node-id=0%3A1&t=AXnojiyb2kDk9YvM-1`,
+    },
+  },
+  {
+    title: `toktokgo`,
+    description: `toktokgo serves as the newest car service available in the market. Book your ride now for a hassle free and safe journey to your destination at a very affordable fare.`,
+    image: `/images/projects/toktokgo.webp`,
+    links: {
+      web: `https://go.toktok.ph/`,
+      figma: `https://www.figma.com/design/1PW9nT1pB3LqaFNdGUt3JU/toktokgo-Website?node-id=0%3A1&t=8UH5SKHmB22Ko0EK-1`,
+    },
+  },
+  {
+    title: `toktokdelivery`,
+    description: `Connecting people through door-to-door deliveries that are fast, guaranteed safe, and hassle-free! Book 2 wheels to 4 wheels for your delivery needs. Make toktok your go-to buddy for small up to bulk deliveries!`,
+    image: `/images/projects/toktokdelivery.webp`,
+    links: {
+      web: `https://toktok.ph/book/home`,
+      figma: `https://www.figma.com/file/MrMB6H4CHLezYMaMUOjD6Y/toktokdelivery-website?type=design&node-id=0%3A1&mode=design&t=yPms5fLdSh3XKjAN-1`,
+    },
+  },
+  {
+    title: `TheQuestHub v2`,
+    description: `Peer2Peer uncollateral USDC loan marketplace powered by Solana and Polygon.`,
+    image: `/images/projects/thequesthub-v2.webp`,
+    links: {
+      web: ``,
+      figma: `https://www.figma.com/design/BrKlrTMWKxKGJVv3tCPjXp/TheQuestHub-v2?node-id=851%3A171&t=2NhotrlXusVmFNe2-1`,
+    },
+  },
+  {
+    title: `TheQuestHub v1`,
+    description: `lorem ipsum`,
+    image: `/images/projects/thequesthub-v1.webp`,
+    links: {
+      web: ``,
+      figma: `https://www.figma.com/design/VnBTVohMjE7WwabeAfB77d/TheQuestHub?node-id=0%3A1&t=lPJPfwEiK9prYrpu-1`,
+    },
+  },
+  {
+    title: `coffee:life`,
+    description: `lorem ipsum`,
+    image: `/images/projects/coffee-life.webp`,
+    links: {
+      web: `https://www.coffeelife.ph/`,
+      figma: `https://www.figma.com/file/LVf08ixx7AReDOX75YESSD/Coffee-Life-Website?type=design&node-id=0-1&mode=design&t=TXNaSrMf7ad9T7ha-0`,
+    },
+  },
+
+  {
     title: `Yogorino`,
     description: `An e-commerce where customers can easily browse and order their favorite dessert online, providing a seamless and convenient dining experience.`,
     image: `/images/projects/yogorino.webp`,
     links: {
       web: ``,
       figma: `https://www.figma.com/file/ZSznyPbEnfHsuGMFLyeTzJ/yogorino?type=design&node-id=0-1&mode=design&t=KHyt8XKW2vRJzfeo-0`,
-    },
-  },
-  {
-    title: `CopperMask`,
-    description: `A sleek online hub to buy premium masks effortlessly. Browse, select, and purchase with ease. Welcome to our streamlined mask marketplace.`,
-    image: `/images/projects/coppermask.webp`,
-    links: {
-      web: ``,
-      figma: `https://www.figma.com/file/GuksEJW3TDCPgpnbf4gz6N/CopperMask-Website-Revamp?type=design&node-id=0-1&mode=design`,
     },
   },
   {
@@ -83,7 +175,7 @@ export const projectsData: Project[] = [
   },
   {
     title: `My Portfolio v1`,
-    description: `lorem ipsum`,
+    description: `My very first portfolio website helped me land my first job as a Front-End Web Developer.`,
     image: `/images/projects/my-portfolio-v1.webp`,
     links: {
       web: ``,
@@ -100,102 +192,12 @@ export const projectsData: Project[] = [
     },
   },
   {
-    title: `Pristine`,
-    description: `Bitcoin-backed loans platform that features Bitcoin Mortgage, Multi-sig Loans, and Trustless Lightning Loans.`,
-    image: `/images/projects/pristine.webp`,
-    links: {
-      web: `https://nutshell.ph/`,
-      figma: `https://www.figma.com/file/11rGtHrWZ12ZyRi6MzckQd/Pristine?type=design&node-id=0%3A1&mode=design&t=2pj9zw0YMTHeCIX5-1`,
-    },
-  },
-  {
-    title: `Quest:Citizens`,
-    description: `A collection of 2,500 Quest: Citizens represent the adventure to frontiers equipped with new powers never before seen in Solana.`,
-    image: `/images/projects/quest-citizens.webp`,
-    links: {
-      web: ``,
-      figma: ``,
-    },
-  },
-  {
-    title: `TheQuestHub v2`,
-    description: `Peer2Peer uncollateral USDC loan marketplace powered by Solana and Polygon.`,
-    image: `/images/projects/thequesthub-v2.webp`,
-    links: {
-      web: ``,
-      figma: `https://www.figma.com/design/BrKlrTMWKxKGJVv3tCPjXp/TheQuestHub-v2?node-id=851%3A171&t=2NhotrlXusVmFNe2-1`,
-    },
-  },
-  {
-    title: `TheQuestHub v1`,
-    description: `lorem ipsum`,
-    image: `/images/projects/thequesthub-v1.webp`,
-    links: {
-      web: ``,
-      figma: `https://www.figma.com/design/VnBTVohMjE7WwabeAfB77d/TheQuestHub?node-id=0%3A1&t=lPJPfwEiK9prYrpu-1`,
-    },
-  },
-  {
     title: `Siomai King`,
     description: `lorem ipsum`,
     image: `/images/projects/siomai-king.webp`,
     links: {
       web: `https://siomaiking.ph/`,
       figma: ``,
-    },
-  },
-  {
-    title: `toktok`,
-    description: `lorem ipsum`,
-    image: `/images/projects/toktok.webp`,
-    links: {
-      web: `https://toktok.ph/`,
-      figma: `https://www.figma.com/design/UsmhBDIK2sFzzJpRzByjRL/toktoksuper-Website?node-id=0%3A1&t=vsK2QxOdJaFTicPM-1`,
-    },
-  },
-  {
-    title: `toktokmall`,
-    description: `lorem ipsum`,
-    image: `/images/projects/toktokmall.webp`,
-    links: {
-      web: `https://toktokmall.ph/`,
-      figma: `https://www.figma.com/design/yU3uAQJrORAb0fdjisrHHp/toktokmall-web?node-id=1%3A148&t=lBwz46xx5dl4gmfe-1`,
-    },
-  },
-  {
-    title: `toktokfood`,
-    description: `lorem ipsum`,
-    image: `/images/projects/toktokfood.webp`,
-    links: {
-      web: `https://toktokfood.ph/`,
-      figma: ``,
-    },
-  },
-  {
-    title: `toktokwallet`,
-    description: `lorem ipsum`,
-    image: `/images/projects/toktokwallet.webp`,
-    links: {
-      web: `https://wallet.toktok.ph/`,
-      figma: `https://www.figma.com/design/F8pniqNOskEKk7FRCOAcZz/toktokwallet-Website?node-id=0%3A1&t=AXnojiyb2kDk9YvM-1`,
-    },
-  },
-  {
-    title: `toktokgo`,
-    description: `lorem ipsum`,
-    image: `/images/projects/toktokgo.webp`,
-    links: {
-      web: `https://go.toktok.ph/`,
-      figma: `https://www.figma.com/design/1PW9nT1pB3LqaFNdGUt3JU/toktokgo-Website?node-id=0%3A1&t=8UH5SKHmB22Ko0EK-1`,
-    },
-  },
-  {
-    title: `toktokdelivery`,
-    description: `lorem ipsum`,
-    image: `/images/projects/toktokdelivery.webp`,
-    links: {
-      web: `https://toktok.ph/book/home`,
-      figma: `https://www.figma.com/file/MrMB6H4CHLezYMaMUOjD6Y/toktokdelivery-website?type=design&node-id=0%3A1&mode=design&t=yPms5fLdSh3XKjAN-1`,
     },
   },
   {
