2ec4112f7fe9854320a1594b0f894e73f6f51d6f
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2026-07-17 23:18:36 +0800

    chore(background): soften constellation lines in light mode

--- src/components/ConstellationChart.tsx
+++ src/components/ConstellationChart.tsx
@@ -62,7 +62,7 @@ export default function ConstellationChart() {
       viewBox="0 0 1440 900"
       preserveAspectRatio="xMidYMid slice"
     >
-      <g className={`stroke-star`} strokeWidth="0.8">
+      <g className={`stroke-star opacity-[0.12] dark:opacity-30`} strokeWidth="0.8">
         {LINES.map(([x1, y1, x2, y2]) => (
           <line
             key={`${x1}-${y1}-${x2}-${y2}`}
@@ -70,7 +70,6 @@ export default function ConstellationChart() {
             y1={y1}
             x2={x2}
             y2={y2}
-            opacity={0.28}
           />
         ))}
       </g>
